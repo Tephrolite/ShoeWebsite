10bf29ea366bf129aead8345e2bbbf2e3aa945e5
dev <devenphillips7@gmail.com>
2019-08-28 13:17:45 -0500

    cheese

--- fullstack_app/client/src/popular.js
+++ fullstack_app/client/src/popular.js
@@ -3,25 +3,38 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 import './Page.css';
 
 class Users extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="Page">
-                    <div className="Header">
-                        <Link to={'/'}  className="websiteTitleContainer">
-                            <h1 className="websiteTitleText">Chicken Nuggets</h1>
-                        </Link>
-                        <Link to={'/popular'} className="LogIn">
-                            <p className="buttonText">LOG IN</p>
-                        </Link>
-                        <Link to={'/bestseller'} className="SignUp">
-                            <p className="buttonText">SIGN UP</p>
-                        </Link>
-                    </div>
-                    
-                </div>
+
+
+
+   header = () => {
+      return (
+         <div className="Header">
+            <div className="websiteTitleContainer">
+               <h1 className="websiteTitleText">
+                  Chicken Nuggets
+        </h1>
             </div>
-        )
-    }
+            <Link to={'/popular'} className="LogIn">
+               <p className="buttonText">LOG IN</p>
+            </Link>
+            <Link to={'/bestseller'} className="SignUp">
+               <p className="buttonText">SIGN UP</p>
+            </Link>
+         </div>
+      )
+   }
+
+   render() {
+      return (
+         <div className="container">
+            <div className="Page">
+               {this.header()}
+               <div className="pageContent">
+
+               </div>
+            </div>
+         </div>
+      )
+   }
 }
 export default Users
